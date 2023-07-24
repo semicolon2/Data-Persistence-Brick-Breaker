0b3bc2ec003c22c89383e440209f0e43d6804939
semicolon2 <benfallan12@gmail.com>
2023-07-24 17:48:50 -0600

    display high score list

--- Assets/Scripts/HighScoreManager.cs
+++ Assets/Scripts/HighScoreManager.cs
@@ -2,13 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class HighScoreManager : MonoBehaviour
 {
+    public GameObject TextPrefab;
+    public GameObject Canvas;
+    private float nextPostion = -100;
     void Start()
     {
         DataManager.Instance.SortScores();
 
+        foreach (DataManager.HighScore highScore in DataManager.Instance.HighScores)
+        {
+            TMP_Text text = Instantiate(TextPrefab, Canvas.transform.position, transform.rotation).GetComponent<TMP_Text>();
+            text.transform.SetParent(Canvas.transform, false);
+            text.rectTransform.anchoredPosition = new Vector2(0, nextPostion);
+            text.text = $"1. {highScore.PlayerName} : {highScore.Score}";
+            nextPostion -= 50;
+        }
     }
 
     public void ReturnToMenu()
