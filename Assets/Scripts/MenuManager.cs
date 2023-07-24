8e38e9bfa398b2a8e295288eea1eba4fdc64a1bc
semicolon2 <benfallan12@gmail.com>
2023-07-24 10:56:02 -0600

    UI button functionality

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 
 public class MenuManager : MonoBehaviour
 {
@@ -24,5 +26,24 @@ public class MenuManager : MonoBehaviour
     public void ReturnToMenu()
     {
         SceneManager.LoadScene(0);
+    }
+
+    public void StartGameButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void HighScoreButton()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Quit()
+    {
+        #if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+        #else
+        Application.Quit();
+        #endif
     }
 }
